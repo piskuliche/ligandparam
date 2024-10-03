174e5f33394844ece38374c84c40da6d487d1c6b
Zeke Piskulich <piskulichz@gmail.com>
2024-10-03 13:27:20 -0400

    Refactor ligand parameterization to include nproc and mem attributes, and integrate GaussianWriter for input generation

--- ligand-param/ligand_param/module.py
+++ ligand-param/ligand_param/module.py
@@ -1,7 +1,7 @@
 import os
 
 from antechamber_interface import Antechamber
-from gaussian_input import GaussianInput
+from gaussian_writer import GaussianInput, GaussianWriter
 
 
 
@@ -9,7 +9,7 @@ class Parametrization:
     """An example class that represents a simple entity."""
 
 
-    def __init__(self, pdb_file, netcharge=None, atom_type=None, theory_low='HF/6-31G*', theory_high='PBE1PBE/6-31G*'):
+    def __init__(self, pdb_file, netcharge=None, atom_type=None, theory_low='HF/6-31G*', theory_high='PBE1PBE/6-31G*', nproc=6, mem='8GB'):
         """Initialize the class with a PDB file and a net charge.
 
         Parameters
@@ -25,6 +25,9 @@ class Parametrization:
         self.atom_type = atom_type
         self.theory={"low":theory_low, 
                      "high":theory_high}
+        
+        self.nproc = nproc
+        self.mem = mem
 
         # Set None behavior
         if self.net_charge is None:
@@ -38,16 +41,47 @@ class Parametrization:
 
         return
     
-
-class FreeLigand(Parametrization):
     def initialize(self):
+        """
+        Initialize the ligand parameterization process.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        """
+        
         ante = Antechamber()
         ante.call(i=self.pdb_filename, fi = 'pdb',
                   o=self.base_name+'.mol2', fo = 'mol2',
                   c=self.atom_type, nc=self.net_charge, 
                   run=False)
-
+        
+        header = [f'%NPROC={self.nproc}', f'%MEM={self.mem}', '%CHK='+self.base_name+'.antechamber.chk']
+        gau = GaussianWriter(self.base_name+'.com')
+        gau.add_block(GaussianInput(command=f"#P {self.theory['low']} OPT(CalcFC)",
+                                    header=header))
+        gau.add_block(GaussianInput(command=f"#P {self.theory['high']} OPT(CalcFC) GEOM(ALLCheck) Guess(Read)", 
+                                    header=header))
+        gau.add_block(GaussianInput(command=f"#P {self.theory['low']} GEOM(AllCheck) Guess(Read) NoSymm Pop=mk IOp(6/33=2) GFInput GFPrint", 
+                                    header=header))
+        gau.write(dry_run=True)
     
+
+class FreeLigand(Parametrization):
+    def new():
+        pass
+
+class ProteinLigand(Parametrization):
+    def new():
+        pass
+
+class RNALigand(Parametrization):
+    def new():
+        pass
     
 
 
