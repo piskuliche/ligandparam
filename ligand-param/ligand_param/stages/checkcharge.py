1e26625b5678b29b2895a8d136f8e1950e4c5082
Zeke Piskulich <piskulichz@gmail.com>
2024-10-11 11:43:09 -0400

    Enhance CheckCharge class to calculate and validate total charge against net charge

--- ligand-param/ligand_param/stages/checkcharge.py
+++ ligand-param/ligand_param/stages/checkcharge.py
@@ -1,8 +1,11 @@
-from ligand_param.abns
+import numpy as np
+import MDAnalysis as mda
 
-class CheckCharge(AbstractStage):
+from ligand_param.stages.abstractstage import AbstractStage
+
+class StageCheckCharge(AbstractStage):
     """ This is an abstract class for all the stages. """
-    def __init__(self, name, **kwargs) -> None:
+    def __init__(self, name, filename = None, filetype = None) -> None:
         self.name = name
         pass
     
@@ -10,6 +13,15 @@ class CheckCharge(AbstractStage):
         return stage
 
     def _execute(self, dry_run=False):
+        u = mda.Universe(self.filename, format=self.filetype)
+        total_charge = sum(u.atoms.charges)
+        print("Total charge: ", total_charge)
+        compare_charge = self.netcharge - total_charge
+        if np.round(compare_charge, 2) != 0.0:
+            print("Error: Total charge does not match the net charge")
+            print(f"Net charge: {self.netcharge}, Total charge: {total_charge}")
+            print(f"Charge difference: {compare_charge}")
+            raise ValueError("Error: Total charge does not match the net charge")
         return
 
     def _clean(self):
