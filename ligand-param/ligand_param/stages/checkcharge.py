2eaad714a2824ea00d0c5afecbab0f6c3aba4218
Zeke Piskulich <piskulichz@gmail.com>
2024-10-11 11:53:24 -0400

    Fixed bug.

--- ligand-param/ligand_param/stages/checkcharge.py
+++ ligand-param/ligand_param/stages/checkcharge.py
@@ -5,10 +5,11 @@ from ligand_param.stages.abstractstage import AbstractStage
 
 class StageCheckCharge(AbstractStage):
     """ This is an abstract class for all the stages. """
-    def __init__(self, name, filename = None, filetype = None) -> None:
+    def __init__(self, name, filename = None, filetype = None, netcharge=0.0) -> None:
         self.name = name
         self.filename = filename
         self.filetype = filetype
+        self.netcharge = netcharge
         pass
     
     def _append_stage(self, stage: "AbstractStage") -> "AbstractStage":
