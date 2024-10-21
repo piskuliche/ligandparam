d6b93a20a6e3241def20e07b579c4f847298c764
Zeke Piskulich <piskulichz@gmail.com>
2024-10-21 13:26:38 -0400

    Add file listing and output tracking to AbstractStage class

--- ligand-param/ligand_param/stages/abstractstage.py
+++ ligand-param/ligand_param/stages/abstractstage.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from pathlib import Path
 
 class AbstractStage(metaclass=ABCMeta):
     """ This is an abstract class for all the stages. """
@@ -25,7 +26,9 @@ class AbstractStage(metaclass=ABCMeta):
         print("************************************")
         print(f"Executing {self.name}")
         print("************************************")
+        self.starting_files = self.list_files_in_directory(".")
         self._execute(dry_run=dry_run)
+        self.ending_files = self.list_files_in_directory(".")
 
     def clean(self) -> None:
         print("************************************")
@@ -33,3 +36,31 @@ class AbstractStage(metaclass=ABCMeta):
         print("************************************")
         self._clean()
         return
+
+    def _add_output(self, file):
+        """ Add an output file to the stage. 
+
+        This is used by different stages to keep track of the output files that
+        it generates. This allows for easy cleaning of the files.
+        
+        Parameters
+        ----------
+        file : str
+            The file to add to the output list.
+        """
+        if self.outputs is None:
+            self.outputs = []
+        self.outputs.append(file)
+        return
+    
+    def list_files_in_directory(self, directory):
+        """ List all the files in a directory. 
+        
+        Parameters
+        ----------
+        directory : str
+            The directory to list the files from.
+            
+        """
+        return [f.name for f in Path(directory).iterdir() if f.is_file()]
+    
