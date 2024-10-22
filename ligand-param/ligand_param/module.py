6bfb134c24060fb7e4e6a339602b18c8d967d4c9
Zeke Piskulich <piskulichz@gmail.com>
2024-10-22 10:40:29 -0400

    Add BuildLigand class and integrate build stages for gas, aqueous, and target systems

--- ligand-param/ligand_param/module.py
+++ ligand-param/ligand_param/module.py
@@ -10,6 +10,7 @@ from ligand_param.stages.leap import StageLeap
 from ligand_param.stages.parmchk import StageParmChk
 from ligand_param.stages.charge import StageUpdateCharge, StageNormalizeCharge
 from ligand_param.stages.typematching import StageUpdate
+from ligand_param.stages.build_system import StageBuild
 
 
 class Parametrization(Driver):
@@ -24,7 +25,7 @@ class Parametrization(Driver):
     def __init__(self, pdb_file, netcharge=0.0, atom_type="gaff2", 
                  theory_low='HF/6-31G*', theory_high='PBE1PBE/6-31G*', 
                  nproc=6, mem='8GB',
-                leaprc = [],
+                leaprc = [], target_pdb=None,
                 force_gaussian_rerun=False):
         """Initialize the class with a PDB file and a net charge.
 
@@ -46,6 +47,7 @@ class Parametrization(Driver):
         self.theory={"low":theory_low, 
                      "high":theory_high}
         self.force_gaussian_rerun = force_gaussian_rerun
+        self.target_pdb = target_pdb
 
 
         # Settings for Processor Environment
@@ -202,9 +204,47 @@ class FreeLigand(Parametrization):
             StageLeap("Leap", base_cls=self)
         ]
 
-class ProteinLigand(Parametrization):
-    def new():
-        pass
+class BuildLigand(Parametrization):
+    def setup(self):
+        self.target
+        self.stages = [
+            StageInitialize("Initialize", base_cls=self),
+            StageNormalizeCharge("Normalize2", base_cls=self, 
+                                orig_mol2=self.base_name+".antechamber.mol2", 
+                                new_mol2=self.base_name+".antechamber.mol2"),
+            StageGaussian("Minimize", base_cls=self),
+            StageGaussianRotation("Rotate", base_cls=self,
+                                  alpha=[0, 30, 60, 90, 120, 150, 180],
+                                  beta=[0, 30, 60, 90],
+                                  gamma=[0]),
+            StageGaussiantoMol2("GrabGaussianCharge", base_cls=self),
+            StageMultiRespFit("MultiRespFit", base_cls=self),
+            StageUpdateCharge("UpdateCharge", base_cls=self,
+                              orig_mol2=self.base_name+".antechamber.mol2",
+                              new_mol2=self.base_name+".resp.mol2",
+                              charge_source="multistate"),
+            StageNormalizeCharge("Normalize2", base_cls=self, 
+                                orig_mol2=self.base_name+".resp.mol2", 
+                                new_mol2=self.base_name+".resp.mol2"),
+            StageUpdate("UpdateNames", base_cls=self,
+                                orig_mol2=self.base_name+'.antechamber.mol2',
+                                to_update=self.base_name+'.resp.mol2',
+                                new_mol2=self.base_name+'.resp.mol2', 
+                                update_names=True, 
+                                update_types=False,
+                                update_resname=True),
+            StageUpdate("UpdateTypes", base_cls=self,
+                                orig_mol2=self.base_name+'.log.mol2',
+                                to_update=self.base_name+'.resp.mol2',
+                                new_mol2=self.base_name+'.resp.mol2', 
+                                update_names=False, 
+                                update_types=True),
+            StageParmChk("ParmChk", base_cls=self),
+            StageLeap("Leap", base_cls=self),
+            StageBuild("BuildGas", base_cls=self, build_type='gas'),
+            StageBuild("BuildAq", base_cls=self, build_type='aq', concentration=0.14),
+            StageBuild("BuildTarget", base_cls=self, build_type='target', target_pdb=self.target_pdb)
+        ]
 
 class RNALigand(Parametrization):
     def new():
