069076200b70f1e0403d408636983131f0919c4d
Zeke Piskulich <piskulichz@gmail.com>
2024-10-28 13:44:16 -0400

    Add pyproject.toml for modern packaging and remove setup.py configuration

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='ligandparam',
-    version='0.2.0',
-    author='Zeke Piskulich',
-    author_email='piskulichz@gmail.com',
-    description='A ligand parmameterization package for Amber',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/my-python-package',
-    packages=find_packages(),
-    install_requires=open('requirements.txt').read().splitlines(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+setup()
