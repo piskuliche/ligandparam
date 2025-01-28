e75d8130aafe94b357ed1e0da6528b9fb63c9201
pgbarletta <pbarletta@gmail.com>
2025-01-28 18:27:15 -0500

    Updated example 01

--- examples/01_LazyLigand/param_thio.py
+++ examples/01_LazyLigand/param_thio.py
@@ -4,14 +4,19 @@
 from ligandparam.recipes import LazyLigand
 
 inputoptions = {
-    'name': 'thiophenol',
     'net_charge': 0,
     'mem': '60GB',
-    'nproc': 12
+    'nproc': 12,
+    "atom_type": "gaff2",
+    "gaussian_root": "/home/pb777/GAUSSIAN",
+    "gauss_exedir": "/home/pb777/GAUSSIAN/g16/bsd:/home/pb777/GAUSSIAN/g16",
+    "gaussian_binary": "/home/pb777/GAUSSIAN/g16/g16",
+    "gaussian_scratch": "/home/pb777/GAUSSIAN/g16/scratch",
+
 }
 
 # Load the pdb as a instance of the LazyLigand class
-test = LazyLigand(inputoptions=inputoptions)
+test = LazyLigand(name= cwd/"thiophenol.pdb", cwd=cwd, **inputoptions)
 
 # Select the pre-initialized stages for Lazy Ligand
 test.setup()
