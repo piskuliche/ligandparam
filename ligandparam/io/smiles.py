Fix bug in available_names calculation in RenamePDBTypes

ligandparam/io/smiles.py
@@ -85,7 +85,7 @@ class RenamePDBTypes:
             raise ValueError("ERROR: Only two molecules can be compared for reference.")
         st_comm, rf_comm = self.common_atoms()
         codes = [f"C{i}" for i in range(1, 41)]
-        available_names = set(self.mols[0].names()) | set(self.mols[1].names() | set(codes))
+        available_names = set(self.mols[0].names()) | set(self.mols[1].names()) | set(codes)
         new_names = np.zeros_like(self.mols[0].names())
         for i, j in zip(st_comm, rf_comm):
             new_names[i] = self.mols[1].names()[j]