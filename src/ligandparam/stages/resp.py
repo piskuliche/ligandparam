717e83127a84c0ba7d6d659a18cafb69c3702a63
pgbarletta <pbarletta@gmail.com>
2025-02-11 13:47:33 -0500

    Add resname to StageLazyResp
    ... when calling antechamber

--- src/ligandparam/stages/resp.py
+++ src/ligandparam/stages/resp.py
@@ -7,12 +7,11 @@ from ligandparam.interfaces import Antechamber
 
 from ligandparam.multiresp import parmhelper
 from ligandparam.multiresp.residueresp import ResidueResp
-from ligandparam.log import get_logger
 
 
 class StageLazyResp(AbstractStage):
-    """ This class runs a 'lazy' resp calculation based on only
-        a single gaussian output file. """
+    """This class runs a 'lazy' resp calculation based on only
+    a single gaussian output file."""
 
     def __init__(self, stage_name: str, in_filename: Union[Path, str], cwd: Union[Path, str], *args, **kwargs) -> None:
         super().__init__(stage_name, in_filename, cwd, *args, **kwargs)
@@ -20,44 +19,54 @@ class StageLazyResp(AbstractStage):
         self.add_required(self.in_gaussian_log)
         self.out_mol2 = Path(kwargs["out_mol2"])
 
-        self.net_charge = getattr(kwargs, 'net_charge', 0.0)
-        self.atom_type = getattr(kwargs, 'atom_type', "gaff2")
+        self.net_charge = getattr(kwargs, "net_charge", 0.0)
+        self.atom_type = getattr(kwargs, "atom_type", "gaff2")
 
+        self.resname = getattr(kwargs, "resname", "LIG")
 
     def _append_stage(self, stage: "AbstractStage") -> "AbstractStage":
-        """ Appends the stage. """
+        """Appends the stage."""
         return stage
 
     def _execute(self, dry_run=False):
-        """ Execute antechamber to convert the gaussian output to a mol2 file. 
-        
+        """Execute antechamber to convert the gaussian output to a mol2 file.
+
         Parameters
         ----------
         dry_run : bool, optional
             If True, the stage will not be executed, but the function will print the commands that would
         """
         ante = Antechamber(cwd=self.cwd, logger=self.logger, nproc=self.nproc)
-        ante.call(i=self.in_gaussian_log, fi='gout',
-                  o=self.out_mol2, fo='mol2',
-                  gv=0, c='resp', nc=self.net_charge, at=self.atom_type,
-                  gn=f"%nproc={self.nproc}", gm=f"%mem={self.mem}MB",
-                  dry_run=dry_run)
+        ante.call(
+            i=self.in_gaussian_log,
+            fi="gout",
+            o=self.out_mol2,
+            fo="mol2",
+            gv=0,
+            c="resp",
+            nc=self.net_charge,
+            at=self.atom_type,
+            gn=f"%nproc={self.nproc}",
+            gm=f"%mem={self.mem}MB",
+            rn=self.resname,
+            dry_run=dry_run,
+        )
         return
 
     def _clean(self):
-        """ Clean the files generated during the stage. """
+        """Clean the files generated during the stage."""
         raise NotImplementedError("clean method not implemented")
 
 
 class StageMultiRespFit(AbstractStage):
-    """ This class runs a multi-state resp fitting calculation, based on 
-        multiple gaussian output files. 
-        
-        TODO: Implement this class.
-        TODO: Implement the clean method.
-        TODO: Implement the execute method.
-        TODO: Add a check that a multistate resp fit is possible. 
-        """
+    """This class runs a multi-state resp fitting calculation, based on
+    multiple gaussian output files.
+
+    TODO: Implement this class.
+    TODO: Implement the clean method.
+    TODO: Implement the execute method.
+    TODO: Add a check that a multistate resp fit is possible.
+    """
 
     def __init__(self, stage_name: str, in_filename: Union[Path, str], cwd: Union[Path, str], *args, **kwargs) -> None:
         super().__init__(stage_name, in_filename, cwd, *args, **kwargs)
@@ -68,12 +77,11 @@ class StageMultiRespFit(AbstractStage):
         self.out_mol2 = Path(kwargs["out_mol2"])
         self.add_required(self.out_mol2)
         self.out_respfit = Path(kwargs["out_respfit"])
-        
-        self.net_charge = getattr(kwargs, 'net_charge', 0.0)
 
+        self.net_charge = getattr(kwargs, "net_charge", 0.0)
 
     def _append_stage(self, stage: "AbstractStage") -> "AbstractStage":
-        """ Appends the stage. """
+        """Appends the stage."""
         return stage
 
     def _execute(self, dry_run=False):
@@ -111,5 +119,5 @@ class StageMultiRespFit(AbstractStage):
         return
 
     def _clean(self):
-        """ Clean the files generated during the stage. """
+        """Clean the files generated during the stage."""
         raise NotImplementedError("clean method not implemented")
