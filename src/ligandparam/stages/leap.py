62f6ea1370ec7af8986d5a7aac8d66bed29691c0
pgbarletta <pbarletta@gmail.com>
2025-02-11 13:42:36 -0500

    Add pdb output to tleap stage

--- src/ligandparam/stages/leap.py
+++ src/ligandparam/stages/leap.py
@@ -1,50 +1,46 @@
-import warnings
 from typing import Union
 
-import MDAnalysis as mda
 from pathlib import Path
 
 from ligandparam.stages.abstractstage import AbstractStage
 from ligandparam.io.leapIO import LeapWriter
 from ligandparam.interfaces import Leap
-from ligandparam.log import get_logger
 from ligandparam.utils import find_word_and_get_line
 
 
 class StageLeap(AbstractStage):
-    """ This is class to run a basic leap calculations on the ligand. """
-
     def __init__(self, stage_name: str, in_filename: Union[Path, str], cwd: Union[Path, str], *args, **kwargs) -> None:
         super().__init__(stage_name, in_filename, cwd, *args, **kwargs)
         self.in_mol2 = Path(in_filename)
         self.add_required(self.in_mol2)
         self.in_frcmod = kwargs["in_frcmod"]
         self.add_required(self.in_frcmod)
-        self.out_lib = kwargs["out_lib"]
+        self.out_lib = Path(kwargs["out_lib"])
+        # Write a PDB as well
+        self.out_pdb = Path(self.out_lib.parent, f"parametrized_{self.out_lib.stem}.pdb")
 
         self.leaprc = kwargs.get("leaprc", ["leaprc.gaff2"])
 
     def _append_stage(self, stage: "AbstractStage") -> "AbstractStage":
-        """ Appends the stage. """
+        """Appends the stage."""
         return stage
 
     def _execute(self, dry_run=False):
-        """ Setup and execute the leap lib file generation """
+        """Setup and execute the leap lib file generation"""
         # Generate the leap input file
         leapgen = LeapWriter("param")
         # Add the leaprc files
         for rc in self.leaprc:
             leapgen.add_leaprc(rc)
 
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            u = mda.Universe(self.in_mol2)
-        # Used to be: `resname = u.residues.resnames[0]`, but sometimes the resname is just a number, tripping tleap
+        # resname might become user settable later
         resname = "LIG"
         # Add the leap commands
         leapgen.add_line(f"loadamberparams {self.in_frcmod.name}")
         leapgen.add_line(f"{resname} = loadmol2 {self.in_mol2.name}")
+        leapgen.add_line(f'set {resname}.1 name "{resname}"')
         leapgen.add_line(f"saveOff {resname} {self.out_lib}")
+        leapgen.add_line(f"savePDB {resname} {self.out_pdb}")
         leapgen.add_line("quit")
         # Write the leap input file
         leapgen.write(self.cwd / "tleap.param.in")
