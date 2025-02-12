2bf3c9c5c2b8a4fb922c76eef5e2fc5e05f0baf4
pgbarletta <pbarletta@gmail.com>
2025-02-12 17:26:42 -0500

    Update example to new ligandparam

--- examples/01_LazyLigand/param_thio.py
+++ examples/01_LazyLigand/param_thio.py
@@ -1,30 +1,57 @@
 #!/usr/bin/env python
 from pathlib import Path
+import logging
 
 # Import the module
 from ligandparam.recipes import LazyLigand
 
-inputoptions = {
-    'net_charge': 0,
-    'mem': '60GB',
-    'nproc': 12,
-    "atom_type": "gaff2",
+# Helper function to set up a file logger
+def set_file_logger(logfilename: Path, logname: str = None, filemode: str = 'a') -> logging.Logger:
+    if logname is None:
+        logname = Path(logfilename).stem
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    file_handler = logging.FileHandler(filename=logfilename, mode=filemode)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+cwd = Path(".").resolve()
+
+# Environment variables for Gaussian. If your environment is already set up, you can ignore this.
+gaussian_paths = {
     "gaussian_root": "/home/pb777/GAUSSIAN",
     "gauss_exedir": "/home/pb777/GAUSSIAN/g16/bsd:/home/pb777/GAUSSIAN/g16",
     "gaussian_binary": "/home/pb777/GAUSSIAN/g16/g16",
     "gaussian_scratch": "/home/pb777/GAUSSIAN/g16/scratch",
-
 }
 
-cwd = Path("./examples/01_LazyLigand/")
-# Load the pdb as a instance of the LazyLigand class
-test = LazyLigand(name=cwd / "thiophenol.pdb", cwd=cwd, **inputoptions)
+logger = set_file_logger(cwd / "lazyligand.log", filemode="w")
+parametrize_ligand = LazyLigand(
+    in_filename=cwd / "thiophenol.pdb",
+    cwd=cwd,
+    logger=logger,
+    net_charge=0,
+    nproc=12,
+    atom_type="gaff2",
+    mem="8192",
+    **gaussian_paths,
+)
 
-# Select the pre-initialized stages for Lazy Ligand
-test.setup()
+# Set the pre-initialized stages for Lazy Ligand
+parametrize_ligand.setup()
 
 # List the stages out to the user
-test.list_stages()
+parametrize_ligand.list_stages()
 
-# Execute the stages in order.
-test.execute(dry_run=False)
+# Run the parametrization
+parametrize_ligand.execute(dry_run=False)
+
