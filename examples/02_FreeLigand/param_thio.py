d9af9e98732776c9cfac0ae1119c3a049321f062
pgbarletta <pbarletta@gmail.com>
2025-02-13 17:25:02 -0500

    New example 02

--- examples/02_FreeLigand/param_thio.py
+++ examples/02_FreeLigand/param_thio.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python
+from pathlib import Path
+import logging
 
 # Import the module
 from ligandparam.recipes import FreeLigand
 
-inputoptions = {
-    'name': 'thiophenol',
-    'net_charge': 0,
-    'mem': '60GB',
-    'nproc': 12
-}
+
+# Helper function to set up a file logger
+def set_file_logger(logfilename: Path, logname: str = None, filemode: str = 'a') -> logging.Logger:
+    if logname is None:
+        logname = Path(logfilename).stem
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    file_handler = logging.FileHandler(filename=logfilename, mode=filemode)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+cwd = Path(".").resolve()
 
 # Load the pdb as a instance of the FreeLigand class
-test = FreeLigand(inputoptions = inputoptions)
+logger = set_file_logger(cwd / "freeligand.log", filemode="w")
+parametrize_ligand = FreeLigand(
+    in_filename=cwd / "thiophenol.pdb",
+    cwd=cwd,
+    logger=logger,
+    net_charge=0,
+    nproc=24,
+    atom_type="gaff2",
+    mem="20480",
+    # **gaussian_paths,
+)
 
 # Select the pre-initialized stages for Lazy Ligand
-test.setup()
+parametrize_ligand.setup()
 
 # List the stages out to the user
-test.list_stages()
+parametrize_ligand.list_stages()
 
 # Execute the stages in order.
-test.execute(dry_run=False)
+parametrize_ligand.execute(dry_run=False)
