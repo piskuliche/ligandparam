da4fd973483d61792ecae53fdbc377b3d5f5447e
pgbarletta <pbarletta@gmail.com>
2025-02-18 09:15:53 -0500

    Clean up lazyligand

--- src/ligandparam/recipes/lazyligand.py
+++ src/ligandparam/recipes/lazyligand.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, final
 
 from typing_extensions import override
 
@@ -8,10 +8,10 @@ from ligandparam.stages import *
 
 
 class LazyLigand(Recipe):
-    """ This is a class for parametrizing a simple ligand using Gaussian and Antechamber.
-    
+    """This is a class for parametrizing a simple ligand using Gaussian and Antechamber.
+
     This class is designed to do a quick parametrization of a very standard ligand. If your
-    ligand is weird in any way, you should use a different class. This class does a very simple 
+    ligand is weird in any way, you should use a different class. This class does a very simple
     parametrization using Gaussian and Antechamber. The steps are:
     1. Initialize the ligand using the PDB file.
     2. Minimize the ligand using Gaussian at a low level of theory.
@@ -34,8 +34,10 @@ class LazyLigand(Recipe):
                 raise ValueError(f"ERROR: Please provide {opt} option as a keyword argument.")
         # required options with defaults
         # TODO: defaults should be a global singleton dict
-        for opt, default_val in zip(("theory", "leaprc", "force_gaussian_rerun"),
-                                    ({"low": "HF/6-31G*", "high": "PBE1PBE/6-31G*"}, ["leaprc.gaff2"], False)):
+        for opt, default_val in zip(
+            ("theory", "leaprc", "force_gaussian_rerun"),
+            ({"low": "HF/6-31G*", "high": "PBE1PBE/6-31G*"}, ["leaprc.gaff2"], False),
+        ):
             try:
                 setattr(self, opt, kwargs[opt])
                 del kwargs[opt]
@@ -49,37 +51,65 @@ class LazyLigand(Recipe):
         self.kwargs = kwargs
 
     def setup(self):
+        initial_mol2 = self.cwd / f"{self.label}.initial.mol2"
+        minimization_gaussian_log = self.cwd / f"{self.label}.minimization.log"
+        resp_mol2 = self.cwd / f"{self.label}.resp.mol2"
+        final_mol2 = self.cwd / f"{self.label}.mol2"
+        frcmod = self.cwd / f"{self.label}.frcmod"
+        lib = self.cwd / f"{self.label}.lib"
+
         self.stages = [
-            StageInitialize("Initialize", in_filename=self.in_filename, cwd=self.cwd,
-                            out_mol2=self.cwd / f"{self.label}.antechamber.mol2", **self.kwargs),
-            StageNormalizeCharge("Normalize1", cwd=self.cwd, net_charge=self.net_charge, **self.kwargs,
-                                 in_filename=self.cwd / f"{self.label}.antechamber.mol2",
-                                 out_mol2=self.cwd / f"{self.label}.antechamber.mol2"),
-            StageGaussian("Minimize", cwd=self.cwd, nproc=self.nproc, mem=self.mem,
-                          gaussian_root=self.gaussian_root, gauss_exedir=self.gauss_exedir,
-                          gaussian_binary=self.gaussian_binary, gaussian_scratch=self.gaussian_scratch,
-                          net_charge=self.net_charge, theory=self.theory,
-                          force_gaussian_rerun=self.force_gaussian_rerun,
-                          in_filename=self.in_filename,
-                          out_gaussian_log=self.cwd / f"{self.label}.log", **self.kwargs),
-            StageLazyResp("LazyResp", cwd=self.cwd, **self.kwargs,
-                          in_filename=self.cwd / f"{self.label}.log",
-                          out_mol2=self.cwd / f"{self.label}.resp.mol2"),
-            StageNormalizeCharge("Normalize2", cwd=self.cwd, net_charge=self.net_charge, **self.kwargs,
-                                 in_filename=self.cwd / f"{self.label}.resp.mol2",
-                                 out_mol2=self.cwd / f"{self.label}.resp.mol2"),
-            StageUpdate("UpdateNames", cwd=self.cwd,
-                        in_filename=self.cwd / f"{self.label}.antechamber.mol2",
-                        to_update=self.cwd / f"{self.label}.resp.mol2",
-                        out_mol2=self.cwd / f"{self.label}.resp.mol2",
-                        update_names=True,
-                        update_types=False,
-                        update_resname=True, **self.kwargs),
-            StageParmChk("ParmChk", in_filename=self.cwd / f"{self.label}.resp.mol2",
-                         out_frcmod=self.cwd / f"{self.label}.frcmod", cwd=self.cwd, **self.kwargs),
-            StageLeap("Leap", in_filename=self.cwd / f"{self.label}.resp.mol2",
-                      in_frcmod=self.cwd / f"{self.label}.frcmod", out_lib=self.cwd / f"{self.label}.lib",
-                      cwd=self.cwd, **self.kwargs)
+            StageInitialize(
+                "Initialize", in_filename=self.in_filename, cwd=self.cwd, out_mol2=initial_mol2, **self.kwargs
+            ),
+            StageNormalizeCharge(
+                "Normalize1",
+                cwd=self.cwd,
+                net_charge=self.net_charge,
+                **self.kwargs,
+                in_filename=initial_mol2,
+                out_mol2=initial_mol2,
+            ),
+            StageGaussian(
+                "Minimize",
+                cwd=self.cwd,
+                nproc=self.nproc,
+                mem=self.mem,
+                gaussian_root=self.gaussian_root,
+                gauss_exedir=self.gauss_exedir,
+                gaussian_binary=self.gaussian_binary,
+                gaussian_scratch=self.gaussian_scratch,
+                net_charge=self.net_charge,
+                theory=self.theory,
+                force_gaussian_rerun=self.force_gaussian_rerun,
+                in_filename=initial_mol2,
+                out_gaussian_log=minimization_gaussian_log,
+                **self.kwargs,
+            ),
+            StageLazyResp(
+                "LazyResp", cwd=self.cwd, **self.kwargs, in_filename=minimization_gaussian_log, out_mol2=resp_mol2
+            ),
+            StageNormalizeCharge(
+                "Normalize2",
+                cwd=self.cwd,
+                net_charge=self.net_charge,
+                **self.kwargs,
+                in_filename=resp_mol2,
+                out_mol2=resp_mol2,
+            ),
+            StageUpdate(
+                "UpdateNames",
+                cwd=self.cwd,
+                in_filename=resp_mol2,
+                source_mol2=initial_mol2,
+                out_mol2=final_mol2,
+                update_names=True,
+                update_types=False,
+                update_resname=True,
+                **self.kwargs,
+            ),
+            StageParmChk("ParmChk", in_filename=final_mol2, out_frcmod=frcmod, cwd=self.cwd, **self.kwargs),
+            StageLeap("Leap", in_filename=final_mol2, in_frcmod=frcmod, out_lib=lib, cwd=self.cwd, **self.kwargs),
         ]
 
     @override
