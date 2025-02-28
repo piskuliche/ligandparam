8217cb8dbc7fb7dc625cfad9b773c21a0ca43976
pgbarletta <pbarletta@gmail.com>
2025-02-28 17:55:09 -0500

    Better lazierligand

--- src/ligandparam/recipes/lazierligand.py
+++ src/ligandparam/recipes/lazierligand.py
@@ -12,9 +12,9 @@ class LazierLigand(Recipe):
 
     This class is designed to do a quick parametrization of a very standard ligand. If your
     ligand is weird in any way, you should use a different class. This class does a very simple
-    parametrization using Gaussian and Antechamber. The steps are:
-    1. Use Antechamber to get bcc charges on a mol2 file out of the starting PDB.
-    2. Check the parameters using ParmChk.
+    parametrization using Antechamber. The steps are:
+    1. Use Antechamber to get bcc or abcg2 charges on a mol2 file out of the starting PDB.
+    2. Check missing parameters using ParmChk.
     3. Generate the Leap input files.
     """
 
@@ -22,24 +22,28 @@ class LazierLigand(Recipe):
     def __init__(self, in_filename: Union[Path, str], cwd: Union[Path, str], *args, **kwargs):
         super().__init__(in_filename, cwd, *args, **kwargs)
 
-        # required options
-        for opt in ("net_charge", "nproc"):
+        for opt, default_val in zip(("net_charge", "nproc"), (0, 1)):
             try:
                 setattr(self, opt, kwargs[opt])
                 del kwargs[opt]
             except KeyError:
-                raise ValueError(f"ERROR: Please provide {opt} option as a keyword argument.")
+                setattr(self, opt, default_val)
+
         self.kwargs = kwargs
 
     def setup(self):
+        nonminimized_mol2 = self.cwd / f"{self.label}.mol2"
+        frcmod = self.cwd / f"{self.label}.frcmod"
+        lib = self.cwd / f"{self.label}.lib"
+        final_mol2 = self.cwd / f"final_{self.label}.mol2"
+
         self.stages = [
             StageInitialize("Initialize", input=self.in_filename, cwd=self.cwd,
-                            out_mol2=self.cwd / f"{self.label}.bcc.mol2", **self.kwargs),
-            StageParmChk("ParmChk", input=self.cwd / f"{self.label}.bcc.mol2",
-                         out_frcmod=self.cwd / f"{self.label}.frcmod", cwd=self.cwd, **self.kwargs),
-            StageLeap("Leap", input=self.cwd / f"{self.label}.bcc.mol2",
-                      in_frcmod=self.cwd / f"{self.label}.frcmod", out_lib=self.cwd / f"{self.label}.lib",
-                      cwd=self.cwd, **self.kwargs)
+                            out_mol2=nonminimized_mol2, **self.kwargs),
+            StageParmChk("ParmChk", input=nonminimized_mol2,
+                         out_frcmod=frcmod, cwd=self.cwd, **self.kwargs),
+            StageLeap("Leap", input=nonminimized_mol2,
+                      in_frcmod=frcmod, out_lib=lib, cwd=self.cwd, **self.kwargs)
         ]
 
     @override
