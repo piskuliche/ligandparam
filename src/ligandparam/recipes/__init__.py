9e67b1da94bf61e226957bf78e3d7aebc94d1ac0
pgbarletta <pbarletta@gmail.com>
2025-03-11 15:13:47 -0400

    Remove unnecesessary imports from non-implemented modules

--- src/ligandparam/recipes/__init__.py
+++ src/ligandparam/recipes/__init__.py
@@ -1,5 +1,3 @@
-from .rnaligand import RNALigand
 from .lazyligand import LazyLigand
 from .lazierligand import LazierLigand
 from .freeligand import FreeLigand
-from .buildligand import BuildLigand
