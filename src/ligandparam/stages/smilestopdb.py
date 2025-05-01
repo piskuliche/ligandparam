760787ed2fe59427cbde94397e02421384f4e769
pgbarletta <pbarletta@gmail.com>
2025-05-01 15:22:24 -0400

    Add reference PDB to StageSmilesToPDB

--- src/ligandparam/stages/smilestopdb.py
+++ src/ligandparam/stages/smilestopdb.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from typing import Optional,  Union, Any
-from typing_extensions import override
-
-from rdkit import Chem
+from typing import Optional, Union, Any
 
 from ligandparam.stages import AbstractStage
+from rdkit import Chem
+from rdkit.Chem import rdFMCS
+from rdkit.Chem.AllChem import ETKDGv3, EmbedMolecule, AlignMol
+from typing_extensions import override
 
+from ligandparam.stages import set_atom_pdb_info
 
 class StageSmilesToPDB(AbstractStage):
 
@@ -19,31 +21,47 @@ class StageSmilesToPDB(AbstractStage):
         self.add_conect = kwargs.get("add_conect", True)
         self.random_seed = kwargs.get("random_seed", None)
 
-    def execute(self, dry_run=False, nproc: Optional[int]=None, mem: Optional[int]=None) -> Any:
+        try:
+            self.reference_pdb = Path(kwargs["reference_pdb"]).resolve()
+            self.add_required(self.reference_pdb)
+            self.normalize_atom_names = True
+            self.align = kwargs.get("align", False)
+        except KeyError:
+            self.normalize_atom_names = False
+            self.align = False
+
+    def execute(self, dry_run=False, nproc: Optional[int] = None, mem: Optional[int] = None) -> Any:
+        super()._setup_execution(dry_run=dry_run, nproc=nproc, mem=mem)
         # First, create the molecule
         try:
             mol = Chem.MolFromSmiles(self.in_smiles)
         except Exception as e:
-            err_msg = f"Failed to generate an rdkit molecule from input SDF {self.in_sdf}. Got exception: {e}"
+            err_msg = f"Failed to generate an rdkit molecule from input SMILES {self.in_smiles}. Got exception: {e}"
             self.logger.error(err_msg)
             raise RuntimeError(err_msg)
 
         if self.reduce:
             mol = Chem.rdmolops.AddHs(mol)
         # All the atoms have their coordinates set to zero. Come up with some values
-        params = Chem.AllChem.ETKDGv3()
+        params = ETKDGv3()
         if self.random_seed:
             params.randomSeed = self.random_seed
-        Chem.AllChem.EmbedMolecule(mol, params)
+        EmbedMolecule(mol, params)
 
-        # Set metadata and write away
-        mol.SetProp("_Name", self.resname)
-        mi = Chem.AtomPDBResidueInfo()
-        mi.SetResidueName(self.resname)
-        mi.SetResidueNumber(1)
-        mi.SetOccupancy(0.0)
-        mi.SetTempFactor(0.0)
-        [a.SetMonomerInfo(mi) for a in mol.GetAtoms()]
+        # Set metadata
+        mol = set_atom_pdb_info(mol, self.resname)
+        # mol.SetProp("_Name", self.resname)
+        # mi = Chem.AtomPDBResidueInfo()
+        # mi.SetResidueName(self.resname)
+        # mi.SetResidueNumber(1)
+        # mi.SetOccupancy(0.0)
+        # mi.SetTempFactor(0.0)
+        # [a.SetMonomerInfo(mi) for a in mol.GetAtoms()]
+
+        # Normalize the molecule to match the reference PDB
+        if self.normalize_atom_names:
+            mol = self.normalize_to_reference(mol, self.reference_pdb, self.align)
+
         flavor = 0 if self.add_conect else 2
         self.logger.info(f"Writing {self.in_smiles} to {self.out_pdb}")
 
@@ -58,3 +76,35 @@ class StageSmilesToPDB(AbstractStage):
 
     def _clean(self):
         raise NotImplementedError
+
+    def normalize_to_reference(self, mol: Chem.Mol, reference_pdb: Path, align: bool = False) -> Chem.Mol:
+        # Normalize the atom names to match the reference PDB
+        ref_mol = Chem.MolFromPDBFile(reference_pdb)
+        if not ref_mol:
+            raise ValueError(f"Failed to read reference PDB file {reference_pdb}")
+
+        mcs_mol = self.get_mcs_mol(ref_mol, mol)
+        ref_substructure = ref_mol.GetSubstructMatch(mcs_mol)
+        common_atms = set(ref_substructure)
+        ref_atoms = list(ref_mol.GetAtoms())
+        for a in mol.GetAtoms():
+            if a.GetIdx() in common_atms:
+                pdb_info = a.GetPDBResidueInfo()
+                name = ref_atoms[a.GetIdx()].GetPDBResidueInfo().GetName()
+                pdb_info.SetName(name)
+                a.SetMonomerInfo(pdb_info)
+
+        if align:
+            mol_substructure = mol.GetSubstructMatch(mcs_mol)
+            assert len(mol_substructure) == len(ref_substructure), \
+                f"Mismatch in number of common atoms. {len(mol_substructure)} vs {len(ref_substructure)}. This is likely a bug."
+            atom_map = list(zip(mol_substructure, ref_substructure))
+            AlignMol(mol, ref_mol, atomMap=atom_map)
+        return mol
+
+    @staticmethod
+    def get_mcs_mol(ref_mol: Chem.Mol, mol: Chem.Mol) -> Chem.Mol:
+        """ Copy the atom names from the reference molecule to the target molecule. """
+        mcs = rdFMCS.FindMCS([ref_mol, mol])
+        common_mol = Chem.rdmolfiles.MolFromSmarts(mcs.smartsString)
+        return common_mol
